5ab52c0205190aa8395b7e4b992a508fd09c9619
Rohan6717 <rohan.techvill@gmail.com>
2023-05-26 00:13:18 +0600

    [feat] Add logo and serch field in header

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import TopHeader from './TopHeader'
-
+import Logo from '../../assets/images/logos/logo.png'
+import SearchField from '../../components/SearchField'
 function Header() {
   return (
     <>
         <TopHeader />
-        <div className='py-2 mx-4 lg:mx-4 xl:mx-32 2xl:mx-64 3xl:mx-92'>
-            
-            <div className='flex'>
-                <div className='w-[20%]'>
-                    dffffffff
-                </div>
-                <div className='w-[40%]'>
-                    dddddddddd
-                </div>
-                <div className='w-[40%]'>
-                    ddddddddd
+        <div className='border-b border-[#dfdfdf]'>
+            <div className='py-4 mx-4 lg:mx-4 xl:mx-32 2xl:mx-64 3xl:mx-92'>
+                <div className='flex'>
+                    <div className='w-[20%]'>
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <div className='w-[40%]'>
+                        <SearchField />
+                    </div>
+                    <div className='w-[40%]'>
+                        
+                    </div>
                 </div>
             </div>
         </div>
