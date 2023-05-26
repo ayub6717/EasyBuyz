a21237e11a035ea78068ed805afbb03b0c74fbe8
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-05-26 23:28:09 +0600

    [feat] Header design and reused dropdown and cartlist

--- src/components/TopHeadBtn.js
+++ src/components/TopHeadBtn.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function TopHeadBtn({name,img,symbol}) {
+function TopHeadBtn(props) {
+  const {name,img,symbol,cartList} = props
   return (
-    <button class="w-fill flex items-center font-dm-sans font-medium">
+    <button className={`${cartList? 'flex flex-col items-center ml-5' : 'w-fill flex items-center font-dm-sans font-medium'}`}>
         <img src={img} alt={img} />
-        <span class="pl-2 text-[#2c2c2c] text-[12px] leading-4"> <span className='text-[16px]'>{symbol}</span> {name}</span>
+        <span className={`${cartList?'text-[10px] text-[#898989] font-medium font-dm-sans mt-[11px] leading-3' : 'pl-2 text-[#2c2c2c] text-[12px] leading-4'}`}> <span className='text-[16px]'>{symbol}</span> {name}</span>
     </button>
   )
 }
--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -2,6 +2,12 @@ import React from 'react'
 import TopHeader from './TopHeader'
 import Logo from '../../assets/images/logos/logo.png'
 import SearchField from '../../components/SearchField'
+import Dropdown from '../../components/Dropdown'
+import dropImg from '../../assets/images/profile/user.png'
+import wishlist from '../../assets/svg/wishlist.svg'
+import cart from '../../assets/svg/cart.svg'
+import trackOrder from '../../assets/svg/trackOrder.svg'
+import TopHeadBtn from '../../components/TopHeadBtn'
 function Header() {
   return (
     <>
@@ -9,14 +15,18 @@ function Header() {
         <div className='border-b border-[#dfdfdf]'>
             <div className='py-4 mx-4 lg:mx-4 xl:mx-32 2xl:mx-64 3xl:mx-92'>
                 <div className='flex'>
-                    <div className='w-[20%]'>
+                    <div className='w-[25%]'>
                         <img src={Logo} alt="logo" />
                     </div>
-                    <div className='w-[40%]'>
+                    <div className='w-[45%]'>
                         <SearchField />
                     </div>
-                    <div className='w-[40%] bg-[#2c2c2c]'>
-                        
+                    <div className='flex items-center justify-end w-[30%]'>
+                        <Dropdown dropImg={dropImg} DropName="Agatha Wilium" />
+                        <TopHeadBtn img={wishlist} name="Wishlist" cartList />
+                        <TopHeadBtn img={cart} name="Your Cart" cartList />
+                        <TopHeadBtn img={trackOrder} name="Track order" cartList />
+                        {/* <Dropdown dropImg={Phone} DropName="Download Our App" Share /> */}
                     </div>
                 </div>
             </div>
