f496f164933108215d693405efd77641e3204117
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-05-27 15:47:22 +0600

    [feat] Menu design and functionalities add for menubar

---
+++ src/components/menu.js
@@ -0,0 +1,17 @@
+export default function Menu() {
+    return (
+        <>
+            <div className="dropdown-menu absolute font-dm-sans">
+                <div className="flex flex-col mt-5 w-44 border text-[#898989]">
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 1</a>
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 2</a>
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 3</a>
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 4</a>
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 5</a>
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 6</a>
+                    <a href className="flex Homes-center justify-between w-full hover:text-[#2c2c2c] hover:bg-[#FCCA19] hover:pl-5 transition-all menuss-hover px-15p py-4 border-b">Home 7</a>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/header/MenuBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState, useRef } from "react";
+import { useOnHoverOutside } from "../../hooks/useOnHoverOutside";
+import Menu from "../../components/menu";
+function MenuBar() {
+    const dropdownRef = useRef(null); // Create a reference for dropdown container
+    const [isMenuDropDownOpen, setMenuDropDownOpen] = useState(false);
+  
+    // Function to close dropdown
+    const closeHoverMenu = () => {
+      setMenuDropDownOpen(false);
+    };
+  
+    useOnHoverOutside(dropdownRef, closeHoverMenu); // Call the hook
+  return (
+    <div className='flex font-dm-sans gap-3'>
+        <div className='border-r pr-2.5' ref={dropdownRef}>
+            <button
+            className="text-[16px] text-[#2c2c2c]"
+            onMouseOver={() => setMenuDropDownOpen(true)} //use mouseover event to show dropdown
+            >
+                Home
+            </button>
+            {isMenuDropDownOpen && <Menu />}
+        </div>
+        <a href className='text-[#2c2c2c] border-r pr-3'>Shop</a>
+        <a href className='text-[#2c2c2c] border-r pr-3'>Coupon</a>
+        <a href className='text-[#2c2c2c] border-r pr-3'>Blogs</a>
+        <a href className='text-[#2c2c2c]'>Pages</a>
+
+    </div>
+  )
+}
+
+export default MenuBar
