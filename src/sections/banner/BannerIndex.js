78c3cd944063f055dec367c01d933ffcc0f9994b
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-05-30 23:25:59 +0600

    [feat] Add banner mixed content

---
+++ src/sections/banner/BannerIndex.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import MixedContent from './MixedContent'
+import ShipIcon from '../../assets/images/banner/ShipIcon.png'
+import CouponInfo from './CouponInfo'
+function BannerIndex() {
+  return (
+    <div className='mx-4 lg:mx-4 xl:mx-32 2xl:mx-64 3xl:mx-92 mt-12'>
+        <div className='flex gap-6'>
+            <div className='flex border border-[#898989] border-dashed'>
+              <MixedContent Mixicon={ShipIcon} Mixtitle="Free Shipping Worldwide" MixtitleTwo="For all orders over $350" />
+              <MixedContent Mixicon={ShipIcon} Mixtitle="Secured Online Payment" MixtitleTwo="For all orders over $350" />
+              <MixedContent Mixicon={ShipIcon} Mixtitle="Money Back Guarantee" MixtitleTwo="For all orders over $350" />
+            </div>
+            <CouponInfo />
+        </div>
+    </div>
+  )
+}
+
+export default BannerIndex
