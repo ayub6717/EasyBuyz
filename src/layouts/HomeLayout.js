c282bd3bee61b6d3d8e9c5a03600b86bce9532e5
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-05-31 00:01:57 +0600

    [feat] Add top category section

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderIndex from '../pages/header/HeaderIndex'
 import BannerIndex from '../sections/banner/BannerIndex'
+import TopCategory from '../sections/top-category/TopCategory'
 
 function HomeLayout() {
   return (
     <div>
         <HeaderIndex />
         <BannerIndex />
+        <TopCategory />
     </div>
   )
 }
---
+++ src/sections/top-category/TopCategory.js
@@ -0,0 +1,104 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from 'react'
+
+function TopCategory() {
+    return (
+        <div>
+            <section className="mx-4 lg:mx-4 xl:mx-32 2xl:mx-64 3xl:mx-92 my-10 md:my-12 font-dm-sans">
+                <p className="text-center font-bold text-sm md:text-[22px] text-gray-12 mb-2.5 md:mb-5 uppercase">
+                    Top Categories Of the Month
+                </p>
+                <div className="grid lg:grid-cols-7 lg:gap-7 grid-flow-col lg:grid-flow-row gap-4 auto-cols-max overflow-auto">
+                    <a href="javascript:void(0)">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/23aec61929155f355dc1887fb712c572.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Electronic Devi...
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="javascript:void(0)">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/cec8ec107035845274d74ce9b509271f.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Electronic Acce...
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="javascript:void(0)">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/4ae2b36e49c0f2e4319e66b880b58bb0.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Health &amp; Beauty
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="babies--toys">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/603bd00bef537fe73d2ab87034ce857e.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Babies &amp; Toys
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="javascript:void(0)">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/d3fb6a0f2345fd8baebb9fe083c56c00.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Fashion
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="javascript:void(0)">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/b4a7f8c45228f03451855b04a92e1e40.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Watches &amp; Acces...
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="javascript:void(0)">
+                        <div className="border hover:bg-[#fcca19] mb-4 md:mb-0 rounded-md relative t-img trans-effect w-130p lg:w-auto">
+                            <div className="flex justify-center items-center h-130p">
+                                <img className="md:h-16 md:w-16 w-66p h-66p object-contain trans-effect" src="https://demo.martvill.techvill.net/public/uploads/20220820/2c5e2203165e9fd500e44cc603f00a40.png" alt="Image" />
+                            </div>
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xs md:text-base mx-3 text-white font-semibold">
+                                <p className="text-gray-12 absolute inset-x-0 bottom-1 md:bottom-3 text-center leading-5 line-clamp-single">
+                                    Automotive &amp; Mo...
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default TopCategory
