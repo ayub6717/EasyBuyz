538e8518fcc630e48ec59d71daaeb68606f48ed8
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-06-01 23:56:53 +0600

    [refactor] [fix] Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,12 +6,33 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import slideOne from "../assets/images/slider/slide-one.png"
+import slideTwo from "../assets/images/slider/slide-two.png"
+import slideThree from "../assets/images/slider/slide-three.png"
+import slideFour from "../assets/images/slider/slide-four.png"
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-export default function Slider(props) {
-  const {slideOne,slideTwo,slideThree,slideFour} = props
+export default function Slider() {
+    const slideImg = [
+      {
+          id:1,
+          url: slideOne,
+      },
+      {
+          id:2,
+          url: slideTwo
+      },
+      {
+          id:3,
+          url: slideThree
+      },
+      {
+          id:3,
+          url: slideFour
+      }
+  ]
   return (
     <>
       <Swiper
@@ -29,18 +50,15 @@ export default function Slider(props) {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>
-            <img src={slideOne} alt={slideOne} />
-        </SwiperSlide>
-        <SwiperSlide>
-            <img src={slideTwo} alt={slideTwo} />
-        </SwiperSlide>
-        <SwiperSlide>
-            <img src={slideThree} alt={slideThree} />
-        </SwiperSlide>
-        <SwiperSlide>
-            <img src={slideFour} alt={slideFour} />
-        </SwiperSlide>
+
+            {slideImg.map((item) => {
+                return (
+                  <SwiperSlide key={item.id}>
+                    <img src={item.url} alt={item.url} />
+                  </SwiperSlide>
+                )
+            })}
+
       </Swiper>
     </>
   );
