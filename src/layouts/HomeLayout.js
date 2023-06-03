852480365c40876191f0f2e9cb7dadd0695fc380
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-06-03 21:00:03 +0600

    [feat]  Add section in home layout

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -5,6 +5,8 @@ import TopCategory from '../sections/top-category/TopCategory'
 import BestDealIndex from '../sections/best-deal/BestDealIndex'
 import ElelctronicIndex from '../sections/Electronics/ElelctronicIndex'
 import ProductBnrIndex from '../sections/banner/ProductBnrIndex'
+import DigiproIndex from '../sections/Digital-product/DigiproIndex'
+import BrandIndex from '../sections/top-brand/BrandIndex'
 
 function HomeLayout() {
   return (
@@ -15,6 +17,8 @@ function HomeLayout() {
         <BestDealIndex /> 
         <ElelctronicIndex />
         <ProductBnrIndex />
+        <DigiproIndex />
+        <BrandIndex />
     </div>
   )
 }
