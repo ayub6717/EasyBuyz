a8e8898634a9671fd3df04369983f3b2a13489d3
Rohan6717 <ayub15-6717@diu.edu.bd>
2023-06-03 20:59:45 +0600

    [feat] Create top brand section

---
+++ src/sections/top-brand/BrandIndex.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+function BrandIndex() {
+    return (
+        <section className="mx-4 lg:mx-4 xl:mx-32 2xl:mx-64 3xl:mx-92 my-10 md:my-12">
+            <p className="dm-bold text-sm text-center md:text-left mb-2.5 md:mb-5 md:text-22 text-gray-12 uppercase">TOP BRANDS</p>
+            <div>
+                <div className="flex flex-col md:flex-row mt-2 md:mt-5 md:gap-x-29p">
+                    <a href="javascript:void(0)" className="relative h-36 md:h-80 md:w-1/4 border rounded-md">
+                        <div className="inset-center">
+                            <div className="grow">
+                                <img className="w-29 h-29 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/74bb8fa91929a6d4e2a587873a91d153.png" alt="" />
+                            </div>
+                        </div>
+                    </a>
+                    <div className="w-full md:w-3/4 mt-5 md:mt-0">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-7">
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/3ddae9aa4c83f0d8fbfb4c3adbcc8289.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/0ad32d1e6b8ffbf2b5d8791440eae4bb.jpg" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/c5aa0f328f80090ed187a30fb1132fa1.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/57c1f5345c241c969ae66d36b32ac4ea.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/a0b3d8fcaf0f51c45dae5b5a6e4a4264.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/15150cf40529fb940eb441d3524a6d7a.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/4a433105500ef52bb552514d6b795f67.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="javascript:void(0)">
+                                <div className="border rounded-md">
+                                    <div className="grow p-6 flex flex-row h-36 justify-center items-center">
+                                        <img className="w-80p h-20 object-contain" src="https://demo.martvill.techvill.net/public/uploads/20221011/b68737e06be2252284f68aed717181c1.png" alt="" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default BrandIndex
