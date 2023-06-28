bb08de093bc284d3a3a3bd4d3653a47ee831db22
Rohan6717 <ayub.devs@gmail.com>
2023-06-28 20:09:48 +0600

    [feat] Fix react 18 warning

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux-box/store/configureStore';
 import './index.css';
 import App from './App';
 import "swiper/css/bundle";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
